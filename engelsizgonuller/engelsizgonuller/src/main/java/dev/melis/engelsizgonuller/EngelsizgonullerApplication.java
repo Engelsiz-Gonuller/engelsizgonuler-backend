0c642348d4141458127e9ce1baeabd377f3815aa
Melis Ölmez <olmezmelis204@gmail.com>
2023-12-29 12:32:26 +0300

    added project file

---
+++ engelsizgonuller/engelsizgonuller/src/main/java/dev/melis/engelsizgonuller/EngelsizgonullerApplication.java
@@ -0,0 +1,13 @@
+package dev.melis.engelsizgonuller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngelsizgonullerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EngelsizgonullerApplication.class, args);
+	}
+
+}
---
+++ engelsizgonuller/engelsizgonuller/src/test/java/dev/melis/engelsizgonuller/EngelsizgonullerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.melis.engelsizgonuller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngelsizgonullerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
