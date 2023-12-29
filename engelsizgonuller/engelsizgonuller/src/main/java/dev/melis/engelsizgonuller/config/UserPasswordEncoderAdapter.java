06c34f7ab7f9701f68c23fd5f74800c9b798db89
Melis Ölmez <olmezmelis204@gmail.com>
2023-12-29 18:14:23 +0300

    added UserPasswordEncoderAdapter

---
+++ engelsizgonuller/engelsizgonuller/src/main/java/dev/melis/engelsizgonuller/config/UserPasswordEncoderAdapter.java
@@ -0,0 +1,26 @@
+package dev.melis.engelsizgonuller.config;
+
+import dev.melis.engelsizgonuller.core.user.UserPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserPasswordEncoderAdapter implements UserPasswordEncoder {
+
+    private final PasswordEncoder passwordEncoder=new BCryptPasswordEncoder();
+    @Override
+    public String encodePassword(String password) {
+        return passwordEncoder.encode(password);
+    }
+
+    @Override
+    public PasswordEncoder getEncoder() {
+        return passwordEncoder;
+    }
+
+    @Override
+    public boolean matces(String password, String passwordHash) {
+        return passwordEncoder.matches(password,passwordHash);
+    }
+}
