5922998f98516d8b767e8e5c229ae040a705d38a
Melis Ölmez <olmezmelis204@gmail.com>
2024-01-06 01:25:32 +0300

    update project

--- engelsizgonuller/engelsizgonuller/src/test/java/dev/melis/engelsizgonuller/EngelsizgonullerApplicationTests.java
+++ engelsizgonuller/engelsizgonuller/src/test/java/dev/melis/engelsizgonuller/EngelsizgonullerApplicationTests.java
@@ -3,7 +3,7 @@ package dev.melis.engelsizgonuller;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class EngelsizgonullerApplicationTests {
 
 	@Test
