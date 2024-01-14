03640ea515497c8f30481d129a80fd2338c40ebc
Melis Ölmez <olmezmelis204@gmail.com>
2024-01-14 23:50:45 +0300

    update assistance requests

--- engelsizgonuller/engelsizgonuller/src/main/java/dev/melis/engelsizgonuller/services/model/helpassistance/AssistanceRequests.java
+++ engelsizgonuller/engelsizgonuller/src/main/java/dev/melis/engelsizgonuller/services/model/helpassistance/AssistanceRequests.java
@@ -24,33 +24,42 @@ public class AssistanceRequests {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "requestId")
-    private  long requestId;
+    private long requestId;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @OnDelete(action = OnDeleteAction.CASCADE)
-    @JoinColumn(name = "userid",nullable = false)
+    @JoinColumn(name = "userid", nullable = false)
     @JsonIgnore
     private User user;
+
     @Column(name = "helpRequesttype")
     private RequestType requestType;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @OnDelete(action = OnDeleteAction.CASCADE)
-    @JoinColumn(name = "categoryid",nullable = false)
+    @JoinColumn(name = "categoryid", nullable = false)
     @JsonIgnore
     private Category category;
 
+    @Column(name = "requestheader")
+    private String requestHeader;
+
     @Column(name = "requestcontent")
     private String requestContent;
+
     @Column(name = "requestdate")
     private LocalDate requestDate;
+
     @Column(name = "requestdeadline")
     @Future
     private LocalDate requestDeadline;
-    @Column(name = "isfulfilled")
-    private boolean isFulFilled=false;
 
-    public AssistanceRequests(){
-        this.requestId=0L;
+    @Column(name = "isfulfilled")
+    @Getter
+    private boolean isFulFilled;
+
+    public AssistanceRequests() {
+        this.requestId = 0L;
+        this.isFulFilled=false;
     }
 }
