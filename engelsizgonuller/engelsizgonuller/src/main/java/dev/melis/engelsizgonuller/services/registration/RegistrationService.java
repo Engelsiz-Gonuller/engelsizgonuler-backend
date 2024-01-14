a1063e4000e9979580f3df80423a0325cb3458a4
Melis Ölmez <olmezmelis204@gmail.com>
2024-01-14 23:59:16 +0300

    update registration service

--- engelsizgonuller/engelsizgonuller/src/main/java/dev/melis/engelsizgonuller/services/registration/RegistrationService.java
+++ engelsizgonuller/engelsizgonuller/src/main/java/dev/melis/engelsizgonuller/services/registration/RegistrationService.java
@@ -1,6 +1,6 @@
 package dev.melis.engelsizgonuller.services.registration;
 
-import dev.melis.engelsizgonuller.business.result.CreationResult;
+import dev.melis.engelsizgonuller.support.result.CreationResult;
 import jakarta.validation.Valid;
 import org.springframework.validation.annotation.Validated;
 
